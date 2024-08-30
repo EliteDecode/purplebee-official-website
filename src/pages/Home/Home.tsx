d45d5256a118bffd6112bece2702ff28b3384235
Gospel Jonathan <sirelite11@gmail.com>
2024-08-30 10:44:01 +0100

    fixed mobile menu

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/home/Hero";
 import Patners from "@/components/home/Patners";
-import LatestProjects from "@/components/home/LatestProjects";
+// import LatestProjects from "@/components/home/LatestProjects";
 import Services from "@/components/home/Services";
 import Globe from "@/components/home/Globe";
 import Testimonials from "@/components/home/Testimonials";
